cb744c41a76388b730d7aa13f7180d69820cf53d
HighKeeperHero <tim.base@heroesveritas.com>
2026-03-20 14:10:39 -0700

    Sprint 25 feat hotfix 4

--- backend/src/landmark/landmark.module.ts
+++ backend/src/landmark/landmark.module.ts
@@ -1,13 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { LandmarkController } from './landmark.controller';
 import { LandmarkService } from './landmark.service';
-import { PrismaService } from '../prisma.service';
 
 @Global()
 @Module({
   imports:     [],
   controllers: [LandmarkController],
-  providers:   [LandmarkService, PrismaService],
+  providers:   [LandmarkService],
   exports:     [LandmarkService],
 })
 export class LandmarkModule {}
