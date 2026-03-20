9af71187c551476387cd3e2708e6f1cbe82118c3
HighKeeperHero <tim.base@heroesveritas.com>
2026-03-20 13:59:55 -0700

    Sprint 25 feat hotfix 3

--- backend/src/landmark/landmark.module.ts
+++ backend/src/landmark/landmark.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { LandmarkController } from './landmark.controller';
 import { LandmarkService } from './landmark.service';
 import { PrismaService } from '../prisma.service';
 
+@Global()
 @Module({
   imports:     [],
   controllers: [LandmarkController],
   providers:   [LandmarkService, PrismaService],
-  exports:     [LandmarkService],   // exported so identity.service.ts can inject it
+  exports:     [LandmarkService],
 })
 export class LandmarkModule {}
